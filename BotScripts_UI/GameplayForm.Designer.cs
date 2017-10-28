587b64bb23e1d2f4d980e078c3b0cfa303ea984c
Mason <norvellmason@gmail.com>
2017-10-27 23:14:13 -0600

    Added resizing capabilities to form.

--- BotScripts_UI/GameplayForm.Designer.cs
+++ BotScripts_UI/GameplayForm.Designer.cs
@@ -28,45 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.PlayerPanel = new System.Windows.Forms.Panel();
+            this.BotPanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // panel1
+            // PlayerPanel
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 673);
-            this.panel1.TabIndex = 0;
+            this.PlayerPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.PlayerPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PlayerPanel.Location = new System.Drawing.Point(0, 0);
+            this.PlayerPanel.Name = "PlayerPanel";
+            this.PlayerPanel.Size = new System.Drawing.Size(200, 673);
+            this.PlayerPanel.TabIndex = 0;
             // 
-            // panel2
+            // BotPanel
             // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(1062, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(200, 673);
-            this.panel2.TabIndex = 1;
+            this.BotPanel.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BotPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.BotPanel.Location = new System.Drawing.Point(1062, 0);
+            this.BotPanel.Name = "BotPanel";
+            this.BotPanel.Size = new System.Drawing.Size(200, 673);
+            this.BotPanel.TabIndex = 1;
             // 
             // GameplayForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1262, 673);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BotPanel);
+            this.Controls.Add(this.PlayerPanel);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "GameplayForm";
             this.Text = "BotScripts";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.GameplayForm_Paint);
+            this.Resize += new System.EventHandler(this.GameplayForm_Resize);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel PlayerPanel;
+        private System.Windows.Forms.Panel BotPanel;
     }
 }
 
